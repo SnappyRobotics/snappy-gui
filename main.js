25be7541cc9de7a54fc98923c136b03ef2468a64
prajankya <prajankya@gmail.com>
2017-03-21 08:21:19 +0530

    updated main with index

--- main.js
+++ main.js
@@ -8,15 +8,24 @@ const {
 
 const path = require('path')
 const url = require('url')
+const fs = require('fs')
+const discovery = require(path.join(__dirname, 'scripts', 'discovery'))
 
-
-
+const debug = require('debug')("snappy:gui:index")
 
 //--------------------------------SETTINGS--------------------------------
 
 global.PORT = 8000
 
 //------------------------------------------------------------------------
+
+global.package = JSON.parse(fs.readFileSync(path.join(__dirname, "package.json")))
+debug("==========================================================================")
+debug("\t\t\t\t\t" + global.package.name)
+debug("\t\t\t\t    " + global.package.description)
+debug("\t\t\t\t\t" + global.package.version)
+debug("==========================================================================")
+
 
 
 
