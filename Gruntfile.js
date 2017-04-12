342b88738fd48ec72cc6ead68ea38401c6ec92ea
prajankya <prajankya@gmail.com>
2017-04-12 13:18:40 +0530

    changed test order

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['test'])
   grunt.registerTask('test', [
     'jshint',
-    'mochaTest:gui',
-    'mochaTest:functions'
+    'mochaTest:functions',
+    'mochaTest:gui'
   ])
 }
