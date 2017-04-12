e980067b737fa1f8c10b7e4e6113917a7f697fad
prajankya <prajankya@gmail.com>
2017-04-12 08:04:17 +0530

    bug fixes

--- scripts/mainWindow.js
+++ scripts/mainWindow.js
@@ -68,14 +68,18 @@ const menuTemplate = [{
         label: 'Reload',
         accelerator: 'CmdOrCtrl+R',
         click(item, focusedWindow) {
-          if (focusedWindow) focusedWindow.reload()
+          if (focusedWindow) {
+            focusedWindow.reload()
+          }
         }
       },
       {
         label: 'Toggle Developer Tools',
         accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
         click(item, focusedWindow) {
-          if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+          if (focusedWindow) {
+            focusedWindow.webContents.toggleDevTools()
+          }
         }
       },
 
